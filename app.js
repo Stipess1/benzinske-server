44a36075dceedf9429262f035cd9265e697d1ecd
Stjepan Stjepanovic <stjepstjepanovic@gmail.com>
2021-01-02 11:27:19 +0100

    compress output

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var request = require('request'),
   zlib = require('zlib');
-  
+
 const express = require('express');
 const app = express();
 
 var headers = {
-  "accept-charset" : "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-  "accept-language" : "en-US,en;q=0.8",
-  "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-  "user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+  "accept-charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+  "accept-language": "en-US,en;q=0.8",
+  "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+  "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
 };
 
 var options = {
@@ -16,39 +16,42 @@ var options = {
   headers: headers,
   "rejectUnauthorized": false
 };
+
 const PORT = process.env.PORT || 3000;
 app.get("/", function(req, res) {
-	var requestWithEncoding = function(options, callback) {
-	  var req = request.get(options);
+  var requestWithEncoding = function(options, callback) {
+    var req = request.get(options);
 
-	  req.on('response', function(res) {
-		var chunks = [];
-		res.on('data', function(chunk) {
-		  chunks.push(chunk);
-		});
+    req.on('response', function(res) {
+      var chunks = [];
+      res.on('data', function(chunk) {
+        chunks.push(chunk);
+      });
 
-		res.on('end', function() {
-		  var buffer = Buffer.concat(chunks);
-		  
-		  zlib.gunzip(buffer, function(err, decoded) {
-			  callback(err, decoded && decoded.toString());
-			});
-		});
-	  });
+      res.on('end', function() {
+        var buffer = Buffer.concat(chunks);
 
-	  req.on('error', function(err) {
-		callback(err);
-	  });
-	}
+        callback(buffer);
+        // zlib.gunzip(buffer, function(err, decoded) {
+        //   callback(err, decoded && decoded.toString());
+        // });
+      });
+    });
 
-	requestWithEncoding(options, function(err, data) {
-			
-		res.setHeader('Content-Type', 'application/json');
-		
-		if (err) console.log(err);
-		else res.end(data);;
-	});
+    req.on('error', function(err) {
+      callback(err);
+    });
+  }
+
+  requestWithEncoding(options, function(data) {
+
+    res.setHeader('Content-Type', 'application/json');
+    if(data.length <= 0)
+        res.sendStatus(500);
+    else {
+      res.end(data);
+    }
+  });
 });
 
 app.listen(PORT);
-
