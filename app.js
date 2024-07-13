1e6a7a6d6e4d189b6fbe81b1fc9fc5f8ccaefc6d
Stjepan <stjepstjepanovic@gmail.com>
2024-07-13 13:15:03 +0200

    cache keys

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var request = require('request');
-var zlib = require('zlib');
+const request = require('request');
+const zlib = require('zlib');
 const NodeCache = require('node-cache');
 const express = require('express');
 const app = express();
@@ -7,21 +7,12 @@ const app = express();
 const myCache = new NodeCache();
 const PORT = process.env.PORT || 3000;
 
-// Function to calculate time remaining until 2 minutes after midnight
-function getTTL() {
-  const now = new Date();
-  const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 2); // Next midnight + 2 minutes
-  return (midnight - now) / 1000; // Return the TTL in seconds
-}
-
-var allowCrossDomain = function (req, res, next) {
+const allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // Intercept OPTIONS method
   if ('OPTIONS' == req.method) {
-    res.send(200);
+    res.sendStatus(200);
   } else {
     next();
   }
@@ -29,84 +20,181 @@ var allowCrossDomain = function (req, res, next) {
 
 app.use(allowCrossDomain);
 
-var headers = {
+const headers = {
   'accept-charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
   'accept-language': 'en-US,en;q=0.8',
   accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
 };
 
-var options = {
+const options = {
   url: 'http://mzoe-gor.hr/data.gz',
   headers: headers,
   rejectUnauthorized: false,
 };
 
-app.get('/', function (req, res) {
-  if (req.headers.authorization === 's7SrVBAkT2WudQW5') console.log('hello');
+function fetchData(callback) {
+  var req = request.get(options);
 
-  // Check if data is in cache
-  let cachedData = myCache.get('cachedData');
-  if (cachedData) {
-    res.end(cachedData);
-    return;
-  }
-
-  var requestWithEncoding = function (options, callback) {
-    var req = request.get(options);
-
-    req.on('response', function (res) {
-      var chunks = [];
-      res.on('data', function (chunk) {
-        chunks.push(chunk);
-      });
-
-      res.on('end', function () {
-        var buffer = Buffer.concat(chunks);
-        zlib.gunzip(buffer, function (err, decoded) {
-          callback(err, decoded && decoded.toString());
-        });
-      });
+  req.on('response', function (res) {
+    var chunks = [];
+    res.on('data', function (chunk) {
+      chunks.push(chunk);
     });
 
-    req.on('error', function (err) {
-      callback(err);
+    res.on('end', function () {
+      var buffer = Buffer.concat(chunks);
+      zlib.gunzip(buffer, function (err, decoded) {
+        callback(err, decoded && decoded.toString());
+      });
     });
-  };
+  });
 
-  requestWithEncoding(options, function (err, data) {
+  req.on('error', function (err) {
+    callback(err);
+  });
+}
+
+function cacheData(key, data) {
+  const ttl = getTTL();
+  myCache.set(key, data, ttl);
+}
+
+function getTTL() {
+  const now = new Date();
+  const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 2);
+  return (midnight - now) / 1000;
+}
+
+// Fetch all data and cache them in different keys
+function fetchAndCacheAllData(callback) {
+  fetchData(function (err, data) {
     if (err) {
-      res.status(500).send('Error fetching data');
+      console.error('Error fetching data:', err);
+      if (callback) callback(err, null);
+      return;
+    }
+    const parsedData = JSON.parse(data);
+
+    // Cache different parts of data in different keys
+    cacheData('allData', parsedData);
+    cacheData('postajas', parsedData['postajas']);
+    cacheData('gorivos', parsedData['gorivos']);
+    cacheData('obvezniks', parsedData['obvezniks']);
+    cacheData('opcijas', parsedData['opcijas']);
+    cacheData('vrsta_danas', parsedData['vrsta_danas']);
+    cacheData('vrsta_gorivas', parsedData['vrsta_gorivas']);
+    cacheData('tip_gorivas', parsedData['tip_gorivas']);
+
+    if (callback) callback(null, parsedData);
+  });
+}
+
+// Cache all data on server start
+fetchAndCacheAllData();
+
+// Refresh cache at 2 minutes after midnight
+setInterval(fetchAndCacheAllData, 24 * 60 * 60 * 1000); // 24 hours
+
+// Middleware to get data from cache or fetch from source
+function getDataFromCacheOrFetch(key, fetchFunction, callback) {
+  const data = myCache.get(key);
+  if (data) {
+    callback(null, data);
+  } else {
+    fetchFunction(function (err, fetchedData) {
+      if (err) {
+        console.error(`Error fetching ${key}:`, err);
+        callback(err, null);
+        return;
+      }
+      cacheData(key, fetchedData);
+      callback(null, fetchedData);
+    });
+  }
+}
+
+// Define routes
+app.get("/", function (req, res) {
+  getDataFromCacheOrFetch('allData', fetchAndCacheAllData, function (err, data) {
+    if (err) {
+      res.status(500).send("Error fetching data");
       return;
     }
 
-    let temp = JSON.parse(data);
+    let temp = JSON.parse(JSON.stringify(data)); // Clone data to avoid mutation
     delete temp['naseljes'];
     delete temp['opcina_grads'];
     delete temp['zupanijas'];
     // neke benzinske postaje imaju krivo postavljene vrste goriva
     for (let i = 0; i < temp['gorivos'].length; i++) {
       let gorivo = temp['gorivos'][i];
-      if ((gorivo['naziv'] === 'EURODIESEL BS' && gorivo['id'] == 29) || gorivo['id'] == 1055) {
+      if (gorivo['naziv'] === "EURODIESEL BS" && (gorivo['id'] == 29 || gorivo['id'] == 1055)) {
         gorivo['vrsta_goriva_id'] = 8;
       } else if (gorivo['id'] == 30) {
         gorivo['vrsta_goriva_id'] = 7;
       }
     }
 
-    const result = JSON.stringify(temp);
-
-    // Cache the result with TTL until 2 minutes after midnight
-    const ttl = getTTL();
-    myCache.set('cachedData', result, ttl);
-    res.end(result);
+    res.json(temp);
   });
 });
 
-app.get('/ping', function (req, res) {
-  res.end('Pinged!');
+app.get("/postajas", function (req, res) {
+  getDataFromCacheOrFetch('postajas', fetchAndCacheAllData, function (err, data) {
+    if (err) {
+      res.status(500).send("Error fetching data");
+      return;
+    }
+    res.json(data);
+  });
 });
 
+app.get("/gorivos", function (req, res) {
+  getDataFromCacheOrFetch('gorivos', fetchAndCacheAllData, function (err, data) {
+    if (err) {
+      res.status(500).send("Error fetching data");
+      return;
+    }
+    res.json(data);
+  });
+});
+
+app.get("/opcijas", function (req, res) {
+  getDataFromCacheOrFetch('opcijas', fetchAndCacheAllData, function (err, data) {
+    if (err) {
+      res.status(500).send("Error fetching data");
+      return;
+    }
+    res.json(data);
+  });
+});
+
+app.get("/:dataType/:id", function (req, res) {
+  const { dataType, id } = req.params;
+  getDataFromCacheOrFetch(dataType, fetchAndCacheAllData, function (err, data) {
+    if (err) {
+      res.status(500).send("Error fetching data");
+      return;
+    }
+   
+    if (!data) {
+      res.status(404).send("Data not found");
+      return 
+    }
+
+    const item = data.find(item => item.id == id);
+    if (!item) {
+      res.status(404).send("Item not found");
+      return;
+    }
+
+    res.json(item);
+  });
+});
+
+// Add more routes for other endpoints as needed...
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
